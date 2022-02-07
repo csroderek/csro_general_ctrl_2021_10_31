8a13f0873d535043d7ac4b40b690e614d26d27c5
cspilitao <litao@cspi.net.cn>
2022-02-07 14:13:19 +0800

    code regen

--- Debug/Core/Startup/subdir.mk
+++ Debug/Core/Startup/subdir.mk
@@ -16,5 +16,12 @@ S_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Core/Startup/%.o: ../Core/Startup/%.s Core/Startup/subdir.mk
-	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -DDEBUG -c -I"C:/Users/Derek/STM32CubeIDE/workspace_1.7.0/csro_general_ctrl_2021_10_31/Csro/fnd_input" -I"C:/Users/Derek/STM32CubeIDE/workspace_1.7.0/csro_general_ctrl_2021_10_31/Csro/fnd_output" -I"C:/Users/Derek/STM32CubeIDE/workspace_1.7.0/csro_general_ctrl_2021_10_31/Modbus_RTU" -I"C:/Users/Derek/STM32CubeIDE/workspace_1.7.0/csro_general_ctrl_2021_10_31/Csro/fnd_com" -I"C:/Users/Derek/STM32CubeIDE/workspace_1.7.0/csro_general_ctrl_2021_10_31/Csro/fnd_eeprom" -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
+	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -DDEBUG -c -I"C:/Users/Litao/STM32CubeIDE/workspace_1.8.0/csro_general_ctrl_2021_10_31/Csro/fnd_input" -I"C:/Users/Litao/STM32CubeIDE/workspace_1.8.0/csro_general_ctrl_2021_10_31/Csro/fnd_output" -I"C:/Users/Litao/STM32CubeIDE/workspace_1.8.0/csro_general_ctrl_2021_10_31/Modbus_RTU" -I"C:/Users/Litao/STM32CubeIDE/workspace_1.8.0/csro_general_ctrl_2021_10_31/Csro/fnd_com" -I"C:/Users/Litao/STM32CubeIDE/workspace_1.8.0/csro_general_ctrl_2021_10_31/Csro/fnd_eeprom" -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
+
+clean: clean-Core-2f-Startup
+
+clean-Core-2f-Startup:
+	-$(RM) ./Core/Startup/startup_stm32f407vetx.d ./Core/Startup/startup_stm32f407vetx.o
+
+.PHONY: clean-Core-2f-Startup
 
